81f41218e4f91260b04559b8ec3756ae7aef4b7a
wangqi <331210877@qq.com>
2017-12-07 21:04:13 +0800

    qwioejioqwurtoqwet

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -6,20 +6,13 @@ use App\Http\Controllers\Controller;
 use DB;
 use Illuminate\Http\Request;
 use Storage;
+use Session;
 
 class IndexController extends Controller
 {
     public function index(){
         if (isset($_GET['code'])){
-            echo $_GET['code'];
-        }else{
-            echo "NO CODE";
-        }
-    }
-
-    public function getUserMessage(Request $request){
-        if (isset($_GET['code'])){
-            echo $_GET['code'];
+            session(['site'=>$_GET['code']]);
         }else{
             echo "NO CODE";
         }
