d21d619bb777a2e593aa3214f0e15c6a9399c393
wangqi <331210877@qq.com>
2017-12-07 18:09:32 +0800

    dwoyioqhjre

--- app/Http/Middleware/WechatMiddleware.php
+++ app/Http/Middleware/WechatMiddleware.php
@@ -16,9 +16,9 @@ class WechatMiddleware
     public function handle($request, Closure $next)
     {
         $app = app('wechat');//从项目实例中得到一个oauth应用实例
-        $oauth = $app->scopes(['snsapi_userinfo'])->redirect();
+        $oauth = $app->oauth;//得到joomla当前用户
         if (!session('open_id')) {//未登录，引导用户到微信服务器授权
-            $oauth->redirect()->send();
+            $response = $app->oauth->scopes(['snsapi_userinfo'])->redirect();
         }else{//已登录状态，重定向到joomla首页
             return $next($request);
         }
