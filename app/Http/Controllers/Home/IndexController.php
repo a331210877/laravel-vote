da75c737530dab427db68da62da2c3c54f2f9724
wangqi <331210877@qq.com>
2017-12-09 10:20:06 +0800

    wqe

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -11,11 +11,9 @@ use Session;
 class IndexController extends Controller
 {
     public function index(Request $request){
-        $app = app('wechat');//从项目实例中得到一个oauth应用实例
-        $oauth = $app->oauth;//得到joomla当前用户
-        $user= $oauth->user();
+        $app = app('wechat');//从项目实例中得到一个oauth应用实例       
+        $user= $app->oauth->setRequest($request)->user();
         dump($app);
-        dump($oauth);
         dump($user);
         dump(session('open_id'));
     }
