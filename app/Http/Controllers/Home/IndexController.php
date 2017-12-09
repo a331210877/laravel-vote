c8271e0391392d6eab7bed7c1caba3d881ac848d
wangqi <331210877@qq.com>
2017-12-09 11:07:51 +0800

    wqe、

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -12,7 +12,7 @@ class IndexController extends Controller
 {
     public function index(Request $request){
         $app = app('wechat');//从项目实例中得到一个oauth应用实例
-        dump($app);
+        // dump($app);
         // $user = $app->oauth->user();
         // dump($request->session);
         // dump(session('open_id'));
