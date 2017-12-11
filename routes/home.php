5b14fda11b11acc5b07c88327b54ba2663a7d4f7
wangqi <331210877@qq.com>
2017-12-11 21:33:23 +0800

    qq

--- routes/home.php
+++ routes/home.php
@@ -6,6 +6,9 @@ Route::group(['middleware' => ['web', 'wechat.oauth']],function(){
 
     // Route::post('/home/index','Home\IndexController@index');
 
-    Route::any('/home/index','Home\IndexController@index');
+    // Route::any('/home/index','Home\IndexController@index');
 
+    Route::any('/home/index', function(){
+        return view('home');
+    });
 });
