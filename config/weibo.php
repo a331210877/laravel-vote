021354443224a99f9e3b7fc8ff5ff3a15109939d
王琦 <331210877@qq.com>
2018-02-03 08:13:08 +0800

    Update weibo.php

--- config/weibo.php
+++ config/weibo.php
@@ -4,7 +4,7 @@ return [
     /*
      * 账号基本信息，请从微信公众平台/开放平台获取
      */
-    'wb_akey'  => env('WB_AKEY', '237166009'),         // AppID
-    'wb_skey'  => env('WB_SKEY', 'bdd0a7b3b9161237ad332ef2ac9bbc78'),     // AppSecret
-    'wb_callback_url'  => env('WB_CALLBACK_URL', 'http://vote.mrwangqi.com/wblogin/callback'),     // callback
+    'wb_akey'  => env('WB_AKEY', ''),         // AppID
+    'wb_skey'  => env('WB_SKEY', ''),     // AppSecret
+    'wb_callback_url'  => env('WB_CALLBACK_URL', ''),     // callback
 ];
